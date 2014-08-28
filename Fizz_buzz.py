a85d0d948b9af61bbf323c5e99b80c34f863c628
Neocles Leontis <nleontis@gmail.com>
2014-08-28 09:19:09 -0400

    Small changes to Fizzbuzz

--- Fizz_buzz.py
+++ Fizz_buzz.py
@@ -13,13 +13,18 @@ elif len(sys.argv) >1:
 		except ValueError:
 			print "No valid integer! will use 100 ..." 
 			n = 100
+
 print "Fizz buzz counting up to ", n
+
 my_list = [i for i in range(0,n+1)]
+
 print my_list
+
 for i in range(1,n+1):
 	if i % 3 == 0 and i % 5 != 0:
 		my_list[i] = "fizz"
-	if i % 5 == 0 and i % 3 != 0:
+	elif i % 5 == 0 and i % 3 != 0:
 		my_list[i] = "buzz"
+
 for i in range(0,n+1):
 	print my_list[i], "\n"
